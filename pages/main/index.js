5b2f5718214ae494992500b832fb858b2cd208f0
Maxim <musaevmaksimm@gmail.com>
2026-04-22 00:47:24 +0300

    fix naming

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,6 +1,11 @@
-import { RequestCardComponent } from "../../components/request-card/index.js";
-import { RequestPage } from "../request/index.js";
-import { get_services } from "../../utils/service-storage.js";
+import { ServiceCardComponent } from "../../components/service-card/index.js";
+import { ButtonComponent } from "../../components/button/index.js";
+import { ServicePage } from "../service/index.js";
+import {
+    create_service_copy_from_first,
+    get_services,
+    remove_service_by_id
+} from "../../utils/service-storage.js";
 
 export class MainPage {
     constructor(parent) {
@@ -18,6 +23,14 @@ export class MainPage {
 
     get empty_state_root() {
         return document.getElementById("service-search-empty");
+    }
+
+    get counter_root() {
+        return document.getElementById("service-counter");
+    }
+
+    get add_service_button_root() {
+        return document.getElementById("add-service-button");
     }
 
     get_filtered_services() {
@@ -49,7 +62,9 @@ export class MainPage {
                         </div>
 
                         <div class="request-counter">
+                            <span id="service-counter">
                             ${services.length} услуги
+                            </span>
                         </div>
                     </div>
 
@@ -62,6 +77,10 @@ export class MainPage {
                             placeholder="Например, сумма или факториал"
                             value="${this.search_query}"
                         >
+                    </div>
+
+                    <div class="mt-3">
+                        <div id="add-service-button" class="d-inline-flex"></div>
                     </div>
                 </div>
 
@@ -78,8 +97,8 @@ export class MainPage {
 
     click_card(event) {
         const service_id = event.target.dataset.id;
-        const request_page = new RequestPage(this.parent, service_id);
-        request_page.render();
+        const service_page = new ServicePage(this.parent, service_id);
+        service_page.render();
     }
 
     handle_search(event) {
@@ -87,19 +106,33 @@ export class MainPage {
         this.update_service_list();
     }
 
+    add_service() {
+        create_service_copy_from_first();
+        this.update_service_list();
+    }
+
+    delete_service(event) {
+        const service_id = Number(event.target.dataset.id);
+        remove_service_by_id(service_id);
+        this.update_service_list();
+    }
+
     update_service_list() {
+        const services = get_services();
         const filtered_services = this.get_filtered_services();
 
         this.request_list_root.innerHTML = "";
 
         filtered_services.forEach((service_data) => {
-            const request_card = new RequestCardComponent(this.request_list_root);
-            request_card.render(
+            const service_card = new ServiceCardComponent(this.request_list_root);
+            service_card.render(
                 service_data,
-                this.click_card.bind(this)
+                this.click_card.bind(this),
+                this.delete_service.bind(this)
             );
         });
 
+        this.counter_root.textContent = `${services.length} услуги`;
         this.empty_state_root.classList.toggle("d-none", filtered_services.length > 0);
     }
 
@@ -108,5 +141,13 @@ export class MainPage {
         this.parent.insertAdjacentHTML("beforeend", this.getHTML());
         this.update_service_list();
         this.search_input.addEventListener("input", this.handle_search.bind(this));
+
+        const add_service_button = new ButtonComponent(this.add_service_button_root);
+        add_service_button.render(
+            "Добавить услугу",
+            "add-service-action",
+            this.add_service.bind(this),
+            "btn btn-danger pm-btn"
+        );
     }
 }
