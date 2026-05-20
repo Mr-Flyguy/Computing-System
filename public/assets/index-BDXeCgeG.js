da025ee2ee2dde1664f7576d1c16648cbd28a909
Maxim <musaevmaksimm@gmail.com>
2026-05-20 04:38:31 +0300

    fix case

--- public/assets/index-BDXeCgeG.js
+++ public/assets/index-BDXeCgeG.js
@@ -3970,7 +3970,7 @@ void main() {
                         </div>
 
                         <div class="request-counter">
-                            <span id="service-counter">${e.length} услуги</span>
+							<span id="service-counter">Кол-во услуг: ${e.length}</span>
                         </div>
                     </div>
 
@@ -3998,4 +3998,4 @@ void main() {
                     По вашему запросу услуги не найдены.
                 </div>
             </div>
-        `}click_card(e){const t=Number(e.target.dataset.id);new sl(this.parent,t).render()}handle_search(e){this.search_query=e.target.value,this.update_service_list()}add_service(){l_(),this.update_service_list()}delete_service(e){const t=Number(e.target.dataset.id);u_(t),this.update_service_list()}update_service_list(){const e=Ir(),t=this.get_filtered_services();this.request_list_root.innerHTML="",t.forEach(n=>{new hl(this.request_list_root).render(n,this.click_card.bind(this),this.delete_service.bind(this))}),this.counter_root.textContent=`${e.length} услуги`,this.empty_state_root.classList.toggle("d-none",t.length>0)}render(){this.parent.innerHTML="",this.parent.insertAdjacentHTML("beforeend",this.getHTML()),this.search_input.addEventListener("input",this.handle_search.bind(this)),new Ps(this.add_service_button_root).render("Добавить услугу","add-service-action",this.add_service.bind(this),"btn btn-danger pm-btn"),this.update_service_list()}}const g_=document.getElementById("root"),__=new rl(g_);__.render();
+		`}click_card(e){const t=Number(e.target.dataset.id);new sl(this.parent,t).render()}handle_search(e){this.search_query=e.target.value,this.update_service_list()}add_service(){l_(),this.update_service_list()}delete_service(e){const t=Number(e.target.dataset.id);u_(t),this.update_service_list()}update_service_list(){const e=Ir(),t=this.get_filtered_services();this.request_list_root.innerHTML="",t.forEach(n=>{new hl(this.request_list_root).render(n,this.click_card.bind(this),this.delete_service.bind(this))}),this.counter_root.textContent=`Кол-во услуг: ${e.length}`,this.empty_state_root.classList.toggle("d-none",t.length>0)}render(){this.parent.innerHTML="",this.parent.insertAdjacentHTML("beforeend",this.getHTML()),this.search_input.addEventListener("input",this.handle_search.bind(this)),new Ps(this.add_service_button_root).render("Добавить услугу","add-service-action",this.add_service.bind(this),"btn btn-danger pm-btn"),this.update_service_list()}}const g_=document.getElementById("root"),__=new rl(g_);__.render();
