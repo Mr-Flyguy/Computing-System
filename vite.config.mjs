714bca2274b92c4ca6e40206c4e00c76706efa72
Maxim <musaevmaksimm@gmail.com>
2026-06-01 01:04:50 +0300

    fix all cards fetch

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    publicDir: false,
     server: {
         proxy: {
             "/calculation_type": "http://localhost:3000"
         }
     },
     build: {
-        outDir: "dist",
+        outDir: "public",
         emptyOutDir: true
     }
 });
