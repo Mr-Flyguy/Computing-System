0736cca524fdf8030a2dfa94d3eca15118f5aec7
Maxim <musaevmaksimm@gmail.com>
2026-06-01 02:54:55 +0300

    add calculation type api helpers

--- modules/calculation-type-api.js
+++ modules/calculation-type-api.js
@@ -6,6 +6,14 @@ export function get_calculation_types(filters = {}) {
     return ajax.get(BASE_URL, filters);
 }
 
+export function post_calculation_type(data) {
+    return ajax.post(BASE_URL, data);
+}
+
+export function patch_calculation_type(id, data) {
+    return ajax.patch(`${BASE_URL}/${id}`, data);
+}
+
 export function get_calculation_type_by_id(id) {
     return ajax.get(`${BASE_URL}/${id}`);
 }
